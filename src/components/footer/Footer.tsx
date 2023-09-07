5f3d428d85f5fc408de85ac45aa1d8ae868e57eb
Ahmed Rmili <ahmedrmili23@gmail.com>
2023-09-07 19:52:22 +0100

    fisrt commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss"
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <span>ahmed</span>
+      <span>ahmed admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
